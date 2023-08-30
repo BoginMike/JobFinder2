d5c6262b050ad91398f2b4b819e68bd76d35ccda
Michael Bogin <120428072+BoginMike@users.noreply.github.com>
2023-08-29 22:46:06 -0400

    Update Index.js

--- src/pages/Index/Index.js
+++ src/pages/Index/Index.js
@@ -1,188 +1,69 @@
 import React, { Fragment } from "react";
 import Header from "../../shared/components/Header";
+import Footer from "../../shared/components/Footer";
 import "./Index.css";
 
 function Index() {
   return (
     <Fragment>
       <Header />
+      <div className="desc1">
+        We RENT Copiers, Fax Machines &amp; Printers for use in
+        Pennsylvania(east of Harrisburg), New Jersey, Delaware (parts of
+        Maryland), Washington, DC, New York (parts of Conn), Virginia and
+        Pittsburgh.
+      </div>
+      <div className="desc2">
+        <div className="desc3">
+          <strong>
+            IN OUR 23rd YEAR!!!!!! We were one of the FIRST and we are the
+            BEST!!!!
+          </strong>
+        </div>
+        <b>Reasons to Rent a Photocopier:</b>
+        <b>
+          <br />
+          1. No maintenance issues. <br />
+          We fix any problems with the machine quickly and free.
+        </b>
+        <b>
+          <br />
+          2. It's much cheaper than buying and the cost can be written off as a
+          business expense.
+        </b>
 
-      <font face="Perpetua" color="#008000" size="6">
-        We RENT Copiers, Fax Machines &amp; Printers for use in{" "}
-      </font>
-      <font face="Perpetua" size="6" color="#CC33FF">
-        Pennsylvania
-      </font>
-      <font face="Perpetua" color="#008000" size="6">
-        ,{" "}
-      </font>
-      <font face="Perpetua" size="6" color="#333399">
-        {" "}
-        New Jersey
-      </font>
+        <b>
+          <br />
+          3. Everything you need to complete your task is included
+          <br />
+          (except paper) toner and copier parts, sorter, stapler, <br />
+          auto feeder and color capability (if required). <br />
+          (we can add scanning, faxing and printer options as well!!!!!)
+        </b>
+        <b>
+          <br />
+          4.&nbsp; We have many flexible rental plans.&nbsp; Just contact us!
+        </b>
+      </div>
 
-      <font face="Perpetua" color="#008000" size="6">
-        ,&nbsp; &nbsp;&nbsp;{" "}
-      </font>
-      <font face="Times New Roman" size="6" color="#0066CC">
-        Delaware
-      </font>
-      <font face="Perpetua" color="#008000" size="6">
-        {" "}
-      </font>
-      <font face="Times New Roman" color="#0066CC" size="6">
-        (parts of Maryland)
-      </font>
-      <font face="Perpetua" color="#008000" size="6">
-        ,&nbsp;{" "}
-      </font>
-      <font face="Times New Roman" color="#990000" size="6">
-        Washington, DC
-      </font>
-      <font face="Perpetua" color="#008000" size="6">
-        {" "}
-        and{" "}
-      </font>
-      <font face="Perpetua" size="6" color="#FF0000">
-        {" "}
-        New York
-      </font>
-      <font face="Perpetua" color="#008000" size="6"></font>
-      <font face="Times New Roman" size="6">
-        (parts of Conn.)
-      </font>
-      <font face="Perpetua" color="#008000" size="6">
-        .
-      </font>
-      <font face="Perpetua" color="#000000" size="5">
-        Call us at{" "}
-      </font>
-      <font face="Perpetua" color="#000000" size="6">
-        609-332-5839
-      </font>
-      <font face="Perpetua" color="#000000" size="5">
-        &nbsp;
-      </font>
-      <font face="Perpetua" color="#000000" size="6">
-        &nbsp;
-      </font>
-      <font face="Perpetua" color="#000000" size="5">
-        OR Email Us at&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <a href="mailto:CopierRental@copymachinerental.com">
-          CopierRental@CopyMachineRental.com
-        </a>
-        <img
-          border="0"
-          src="https://www.copymachinerental.com/Images/animated_mail_box.gif"
-          width="68"
-          height="53"
-          alt="Phila Philly Philadelphia"
-        />
-      </font>
-      <p align="left">&nbsp;</p>
-      <p align="left" className="auto-style1">
-        <strong>
-          IN OUR 23rd YEAR!!!!!! We were one of the FIRST and we are the
-          BEST!!!!
-        </strong>
-      </p>
-      <p align="left">
-        <b>
-          <font face="Perpetua" color="#000000" size="5">
-            Reasons to Rent a Photocopier:
-          </font>
-        </b>
-      </p>
-      <p align="left">
-        &nbsp;&nbsp;&nbsp;
-        <b>
-          <font face="Perpetua" color="#000000" size="5">
-            1.&nbsp; No maintenance issues. We fix any problems with the machine
-            quickly and free.
-          </font>
-        </b>
-      </p>
-      <p align="left">
-        &nbsp;&nbsp;&nbsp;
-        <b>
-          <font face="Perpetua" color="#000000" size="5">
-            2.&nbsp; It's much cheaper than buying and the cost can be written
-            off as a business expense.
-          </font>
-        </b>
-      </p>
-      <p align="left">
-        &nbsp;&nbsp;&nbsp;
-        <b>
-          <font face="Perpetua" color="#000000" size="5">
-            3.&nbsp; Everything&nbsp; you need to complete your task is included
-            (except paper) toner and copier parts, sorter, stapler, auto feeder
-            and color capability (if required). &nbsp;(we can add scanning and
-            printer options as well!!!!!)
-          </font>
-        </b>
-      </p>
-      <p align="left">
-        &nbsp;&nbsp;&nbsp;
-        <b>
-          <font face="Perpetua" color="#000000" size="5">
-            4.&nbsp; We have many flexible rental plans.&nbsp; Just contact us!
-          </font>
-        </b>
-      </p>
-      <p align="left" className="auto-style3">
-        <strong>
-          <em></em>
-        </strong>
-      </p>
-      <p align="left">
-        <b>
-          <font face="Perpetua" color="#000000" size="5">
-            Company Information:
-          </font>
-        </b>
-      </p>
-      <p align="left">
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{" "}
-        <font size="5">
-          <a href="https://WWW.CopyMachineRental.com">
-            WWW.CopyMachineRental.com
-          </a>
-          &nbsp; is a division of Copying Technologies, Inc.&nbsp; We are
-          located conveniently throughout the NY,NJ,DC,PA,VA and DE areas.&nbsp;
-          We are very strong in Philadelphia , Newark , Atlantic City and New
-          York City!Our main office is at 101 E. Main Street in Maple Shade, NJ
-          08052. Ask for Mike.
-        </font>
-      </p>
-      <p align="left">
-        <font face="ArabBruD" size="6" color="#800000">
-          Former Satisfied Clients:
-        </font>
-        <font face="ArabBruD" color="#800000" size="4">
-          1. MTV - Music Television, NY 2. White House Communication Agency -
-          The White House, D.C. 3. NBA Playoffs - NJ Nets - Meadowlands, NJ 4.
-          Harpo Productions 5. Miss America Pageant6. Addidas Corporation7. 2012
-          Democratic National Convention8. NBC and CBS Sports
-        </font>
-      </p>
-      <p align="left">&nbsp;</p>
-      <p align="center">
-        <b>
-          <font face="Perpetua" color="#000000" size="5">
-            <a href="https://www.copymachinerental.com/index.htm#top">
-              Back to top
-            </a>
-          </font>
-        </b>
-      </p>
-      <p align="center">
-        <font face="Photoshop Large" size="5" color="#0000FF">
-          <b>&nbsp;</b>
-        </font>
-      </p>
+      <div className="desc4">
+        Former Satisfied Clients:
+        <div className="desc5">
+          1. MTV - Music Television, NY <br />
+          2. White House Communication Agency - The White House, D.C.
+          <br />
+          3. NBA Playoffs - NJ Nets - Meadowlands, NJ <br />
+          4. Harpo Productions <br />
+          5. Miss America Pageant <br />
+          6. Addidas Corporation <br />
+          7. 2012 Democratic National Convention <br />
+          8. NBC and CBS Sports
+        </div>
+      </div>
+      <Footer />
     </Fragment>
   );
 }
 
 export default Index;
+
