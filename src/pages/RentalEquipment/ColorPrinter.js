6ba19da25d509862561d9fe9cf3d51ffc3aa4cf9
Michael Bogin <120428072+BoginMike@users.noreply.github.com>
2023-08-29 22:26:41 -0400

    commit82923 redid index page

--- src/pages/RentalEquipment/ColorPrinter.js
+++ src/pages/RentalEquipment/ColorPrinter.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState, Fragment } from "react";
-import HPBWLaser from "../Index/webimages/HPLaserJetEntM605NBW.png";
 import { useNavigate } from "react-router-dom";
 import { getApiCall } from "../../shared/api-utils";
 import Header from "../../shared/components/Header";
@@ -22,27 +21,16 @@ function ColorPrinter() {
         setClrPrinter(response);
       }
     });
-  }, []);
+  }, [navigate]);
 
   return (
     <Fragment>
       <Header />
-
-      <font size="6" color="#0000FF">
-        <img
-          border="0"
-          src={HPBWLaser}
-          width="653"
-          height="556"
-          alt="Phila Philly Philadelphia"
-        />
-      </font>
       {clrprinter.map((x) => (
         <div className="ptr-item">
-          {/* <Avatar src={process.env.REACT_APP_BASE_URL + '/image/' + x.albumCover}/> */}
-          {/* <span>
-            <img src={x.copierurl} alt="copier" />{" "}
-          </span> */}
+          <span>
+            <img src={x.clrptrurl} width="600" height="556" alt="copier" />
+          </span>
           <span>
             <pre>{x.clrdescription}</pre>
           </span>
