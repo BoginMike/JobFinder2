dfd363d9a424153f1b7d60c6619825868aa4bff3
Michael Bogin <120428072+BoginMike@users.noreply.github.com>
2023-08-30 19:12:21 -0400

    location fix 2

--- src/pages/LocationsServed/LocationsServed.js
+++ src/pages/LocationsServed/LocationsServed.js
@@ -1,4 +1,5 @@
-import React, { useState, Fragment } from "react";
+import React, { useEffect, useState, Fragment } from "react";
+import { useNavigate } from "react-router-dom";
 import Header from "../../shared/components/Header";
 import "../../App.css";
 import { useForm } from "react-hook-form";
@@ -6,36 +7,54 @@ import { Button, TextField } from "@mui/material";
 import { getApiCall } from "../../shared/api-utils";
 
 function LocationsServed() {
-  const [zips, setZips] = useState([]);
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+  // const [zips, setZips] = useState([]);
+  // const {
+  //   register,
+  //   handleSubmit,
+  //   formState: { errors },
+  // } = useForm();
 
-  function save(data) {
-    console.log("zipsss1");
-    // getApiCall("/zips", data).then((x) => {
-    //   alert("zip added");
-    // });
-    getApiCall("/zips").then((response) => {
-      if (response?.status === true) {
+  // function save(data) {
+  //   console.log("zipsss1");
+  //   // getApiCall("/zips", data).then((x) => {
+  //   //   alert("zip added");
+  //   // });
+  //   getApiCall("/zips").then((response) => {
+  //     if (response?.status === true) {
+  //       setZips(response);
+  //     }
+  //   });
+  //   zips.map((x) => (
+  //     <div className="zip-item">
+  //       {/* <span>{x.zip}</span> */}
+  //       {/* <span>{x.city}</span> */}
+  //       console.log(x.zip);
+  //     </div>
+  //   ));
+  // }
+
+  const [zips, setZips] = useState([]);
+
+  const navigate = useNavigate();
+  useEffect(() => {
+    // on page load this code will ge execute
+    // componentDidMount()
+
+    getApiCall("/bwprinters").then((response) => {
+      if (response?.status === false) {
+        // redirect to login
+        localStorage.clear();
+        navigate("/login");
+      } else {
         setZips(response);
       }
     });
-    zips.map((x) => (
-      <div className="zip-item">
-        {/* <span>{x.zip}</span> */}
-        {/* <span>{x.city}</span> */}
-        console.log(x.zip);
-      </div>
-    ));
-  }
-
+  }, [navigate]);
   return (
     <Fragment>
       <Header />
-      <div className="container">
+
+      {/* <div className="container">
         <form onSubmit={handleSubmit(save)}>
           <TextField label="Zip Code" {...register("zip")} />
           <br />
@@ -44,11 +63,20 @@ function LocationsServed() {
           <br />
           <br />
           {/* <FileUpload onUpload={(n) => setFileName(n)} /> */}
-          <Button variant="contained" type="submit">
+      {/* <Button variant="contained" type="submit">
             Save
           </Button>
-        </form>
-      </div>
+        </form> */}
+      {/* </div> */}
+
+      {zips.map((x) => (
+        <div className="zip-item">
+          <span>{x.zip}</span>
+          <span>
+            <span>{x.city}</span>
+          </span>
+        </div>
+      ))}
     </Fragment>
   );
 }
